a60fe376e188aaae9b440fa8c5c0e88d651d4bb3
Icaro <ikarosouza00@outlook.com>
2020-03-01 19:36:28 -0300

    Section 3: Spinner Component & Refactoring.

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import loader from './static/img/loader.svg';
+
+const Loader = () => {
+    return (
+        <img src={loader} alt="loading"/>
+    );
+};
+
+export default Loader;
